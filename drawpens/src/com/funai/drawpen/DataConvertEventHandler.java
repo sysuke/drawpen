5eff00d6e898569293620cb6b01d792e10d6b8a0
Yosuke Sakate <sakate@funai.co.jp>
2013-03-22 13:19:43 +0900

    いらないファイル削除

--- drawpens/src/com/funai/drawpen/DataConvertEventHandler.java
+++
@@ -1,5 +0,0 @@
-package com.funai.drawpen;
-
-//! @brief 変換に時間がかかる場合のCallback用
-public interface DataConvertEventHandler {
-}
--- drawpens/src/com/funai/drawpen/WifiEventHandler.java
+++
@@ -1,6 +0,0 @@
-package com.funai.drawpen;
-
-public interface WifiEventHandler {
-	
-	public void receiveData(int x, int y, int z, boolean push);
-}
